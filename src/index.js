16a9fb70b5d51504c8f0b8fb905ca00c64e94ee4
Ira Matsuk <g3726866@gmail.com>
2019-03-20 15:02:02 +0300

    feat: add implementation function che.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
+function isPairFound(leftBracket, rightBracket, bracketsConfig){
+  for (var i = 0; i < bracketsConfig.length; i++){
+    var left = bracketsConfig[i][0];
+    var right = bracketsConfig[i][1];
+    if (leftBracket === left && rightBracket === right) {
+      return true;
+    }
+  }
+  return false;
+}
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var arrayFromString = str.split('');
+  if (arrayFromString.length % 2 !== 0){
+    return false;
+  }
+  for (var i = 0; i < arrayFromString.length; i++){
+    if (isPairFound(arrayFromString[i], arrayFromString[i+1], bracketsConfig)) {
+      arrayFromString.splice(i, 2);
+      i = i - 2;
+    }
+  }
+
+  if (arrayFromString.length === 0 ) {
+    return true
+  } else {
+    return false
+  }
 }
